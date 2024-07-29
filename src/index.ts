58724f59c32e5782147bf1985e89ad0c784bfe1e
Karina P <karina_pineda97@hotmail.com>
2024-07-29 02:31:39 -0500

    feat: listener Dom and config

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import { TableController } from "./controllers/TableController";
+
+document.addEventListener('DOMContentLoaded', () => {
+    const tableController = new TableController ('file','search-container');
+})
